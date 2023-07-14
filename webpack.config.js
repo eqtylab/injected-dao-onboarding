aa2cecb02deb7770af7415763a3a2e2b6511bcaf
youfoundron <rongierlach@gmail.com>
2023-07-14 01:10:04 -0700

    use webpack-dev-server, ditch nodemon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const path = require("path");
+const { EnvironmentPlugin } = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
@@ -17,5 +20,23 @@ module.exports = {
   output: {
     filename: "onboarding.js",
     path: path.resolve(__dirname, "example/js"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "head",
+      template: "example/index.html",
+      scriptLoading: "blocking",
+    }),
+    new EnvironmentPlugin([
+      "NODE_ENV",
+      "BASE_URL",
+      "PAYWALL_LOCK_ADDRESS",
+      "MAGIC_PUBLISHABLE_KEY",
+    ]),
+  ],
+  devServer: {
+    static: path.join(__dirname, "example"),
+    compress: true,
+    port: 3000,
   },
 };
